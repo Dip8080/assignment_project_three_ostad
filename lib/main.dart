5b919191c60b0bbfa2ed0f466389e5110abe3f0d
dip8080 <dipmollik7@gmail.com>
2024-05-13 19:19:26 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:assignment_project_three_ostad/myBag/Screen/my_bag.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_sizer/flutter_sizer.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,31 +12,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: MyBag(),
-    );
+    return FlutterSizer(builder: (context, orientation, screenType) {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: MyBag(),
+      );
+    });
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -76,7 +68,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), 
+      ),
     );
   }
 }
--- lib/myBag/Screen/my_bag.dart
+++ lib/myBag/Screen/my_bag.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_sizer/flutter_sizer.dart';
 
 class MyBag extends StatefulWidget {
   const MyBag({super.key});
@@ -31,19 +33,105 @@ class _MyBagState extends State<MyBag> {
       appBar: AppBar(
         actions: [Icon(Icons.search)],
       ),
-      body: Column(children: <Widget>[
-        Text('My Bag',
-            style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold)),
-        Container(
-          child: ListView.builder(
-              itemCount: productData.length,
-              itemBuilder: (BuildContext context, index) {
-                return Container(
-                  child: Text('this is list of ${productData[index]}'),
-                );
-              }),
-        )
-      ]),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text('My Bag',
+                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold)),
+              SizedBox(height: 1.h,),
+              Expanded(
+                child: ListView.builder(
+                    itemCount: productData.length,
+                    itemBuilder: (BuildContext context, index) {
+                      return Container(
+                        child: Row(
+                          children: <Widget>[
+                            Container(
+                              child: Text('image'),
+                            ),
+                            SizedBox(
+                              width: 5.w,
+                            ),
+                            Container(
+                              width: 80.w,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        '${productData[index]['name']}',
+                                        style: TextStyle(
+                                            fontSize: 4.w,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      Icon(
+                                        Icons.more_vert,
+                                        color: Colors.grey,
+                                      )
+                                    ],
+                                  ),
+                                  Row(
+                                    // mainAxisAlignment:
+                                    //     MainAxisAlignment.spaceBetween,
+                                    children: <Widget>[
+                                      RichText(
+                                        text: TextSpan(
+                                          style: TextStyle(),
+                                          children: <TextSpan>[
+                                            TextSpan(
+                                                text: 'Color: ',
+                                                style: TextStyle(
+                                                    color: Colors.grey)),
+                                            TextSpan(
+                                                text:
+                                                    '${productData[index]['color']}',
+                                                style: TextStyle(
+                                                    color: Colors.black,
+                                                    fontWeight:
+                                                        FontWeight.bold))
+                                          ],
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        width: 3.w,
+                                      ),
+                                      RichText(
+                                        text: TextSpan(
+                                          style: TextStyle(),
+                                          children: <TextSpan>[
+                                            TextSpan(
+                                                text: 'Size: ',
+                                                style: TextStyle(
+                                                    color: Colors.grey)),
+                                            TextSpan(
+                                                text:
+                                                    '${productData[index]['size']}',
+                                                style: TextStyle(
+                                                    color: Colors.black,
+                                                    fontWeight:
+                                                        FontWeight.bold))
+                                          ],
+                                        ),
+                                      )
+                                    ],
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      );
+                    }),
+              )
+            ]),
+      ),
     );
   }
 }
